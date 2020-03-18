ea1117e6d9a261d836e545e19d148300460ec080
viktoryia bakun <viktoriabakun00@gmail.com>
2020-03-18 18:39:02 +0300

    fix: improve the condition for a very tricky moment

--- src/what-season.js
+++ src/what-season.js
@@ -1,13 +1,13 @@
-  module.exports = function getSeason(date) {
-    
-    if (date === undefined) {
-        return 'Unable to determine the time of year!';}
+module.exports = function getSeason(date) {
 
-    month = date.getMonth();
-  if (date = Number(date)) {
-      if (month >= 2 && month <= 4) {
-          return 'spring'}
-            else if (month >= 5 && month <= 7) {return 'summer'}
-                  else if (month >= 8 && month <= 10) {return "autumn"}
-                      else {return "winter"}; }
-  };
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }  
+  let month = date.getMonth();
+  if(!(date instanceof Date && !isNaN(date.valueOf()))) throw Error;
+
+  if (month >= 2 && month <= 4) {return 'spring'}
+  else if (month >= 5 && month <= 7) return 'summer';
+  else if (month >= 8 && month <= 10) return 'autumn';
+  else return 'winter';
+}
