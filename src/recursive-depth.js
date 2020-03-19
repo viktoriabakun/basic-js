2bf6f0e03f1cb84774388fc8c5fc5bdcab947e2c
viktoryia bakun <viktoriabakun00@gmail.com>
2020-03-19 17:54:40 +0300

    feat: implement recursive depth calc

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,11 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, acc = 1) {
+      const filtered = arr.filter (arr => Array.isArray(arr)); // we need arrays
+      if (filtered.length === 0) return acc; // no arrays => return accumulator that we already have
+      else {
+          const flats = filtered.map( xs => this.calculateDepth(xs) ) // we replace arrays with the flats of arrays
+          const maxFlats = Math.max(...flats); // ... spreads the array and chooses max element
+          return acc + maxFlats; // return the accumulator that we had + max flats
+      }
     }
 };
