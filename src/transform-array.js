969c794abc92762e9d4393fe115f10c6d06b1df8
viktoryia bakun <viktoriabakun00@gmail.com>
2020-03-19 16:15:40 +0300

    feat: do transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,16 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw Error;
+
+    const controls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!controls.includes(arr[i])) result.push(arr[i]);
+
+        if ('--discard-prev' === arr[i]) result.pop();
+        if ('--double-prev' === arr[i] && arr[i-1] !== undefined) result.push(arr[i-1]);
+        if ('--discard-next' === arr[i] && arr[i+1] !== undefined) i++;
+        if ('--double-next' === arr[i] && arr[i+1] !== undefined) result.push(arr[i+1]);
+    }
+    return result;
 };
