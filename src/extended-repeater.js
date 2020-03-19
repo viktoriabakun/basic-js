d2883bf1e80ce5d312fa3c9cb9abb85b51a147f3
viktoryia bakun <viktoriabakun00@gmail.com>
2020-03-19 19:31:32 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,15 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options ) {
+    let {
+        repeatTimes = 1,
+        separator = "+",
+        addition = "",
+        additionSeparator = "|",
+        additionRepeatTimes = 1,
+      } = options; //destructuring with default values
+      // last adding should be without separator
+      //"some string".repeat(0) == ""
+      const added = (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+      const strAdded = (str + added + separator).repeat(repeatTimes - 1) + (str + added);
+      return strAdded;
 };
   
